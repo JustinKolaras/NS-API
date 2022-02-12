e62f7db3ebec0333782a0f0c28d1a1c03be6ec02
Justin K <justinkolaras@outlook.com>
2022-02-11 20:14:32 -0800

    Debug

--- src/api/middleware/authorization.js
+++ src/api/middleware/authorization.js
@@ -2,9 +2,8 @@ require("dotenv").config({ path: "src/data/.env" });
 
 module.exports = (app) => {
     app.use((req, res, next) => {
-        return res.status(200).send(req.headers);
         const headers = req.headers;
-        if (headers.Authorization && headers.Authorization === process.env.AUTHORIZATION) {
+        if (headers.authorization && headers.authorization === process.env.AUTHORIZATION) {
             next();
         } else {
             return res.status(401).send({
