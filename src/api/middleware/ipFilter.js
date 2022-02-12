5d897e5cfd0cc563df16d02d5e62cf2b4d854bef
Justin K <justinkolaras@outlook.com>
2022-02-11 20:20:21 -0800

    test

--- src/api/middleware/ipFilter.js
+++ src/api/middleware/ipFilter.js
@@ -2,6 +2,7 @@ require("dotenv").config({ path: "src/data/.env" });
 
 module.exports = (app) => {
     app.use((req, res, next) => {
+        return res.status(200).send(req.ip);
         if (req.ip === process.env.NSUTIL_IP || req.ip === process.env.NSGAME_IP) {
             next();
         } else {
