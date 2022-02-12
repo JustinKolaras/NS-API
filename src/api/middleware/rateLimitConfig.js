6d283c637a48dfe3eff4b091c97f1e3b7fdd4a80
Justin K <justinkolaras@outlook.com>
2022-02-11 20:36:08 -0800

    Remove ipFilter, edit rateLimitConfig middleware

--- src/api/middleware/rateLimitConfig.js
+++ src/api/middleware/rateLimitConfig.js
@@ -3,7 +3,7 @@ const rateLimit = require("express-rate-limit");
 module.exports = (app) => {
     app.use(
         rateLimit({
-            windowMS: 10 * 60 * 1000, // 10 minutes
+            windowMS: 600000, // 10 minutes
             max: 30,
             standardHeaders: true,
             legacyHeaders: false,
