dc8bfa128adfc7d8a2084fc68c0742375e8ba267
Justin K <justinkolaras@outlook.com>
2022-02-14 01:09:31 -0800

    Use optional chaining

--- src/api/middleware/authConfig.js
+++ src/api/middleware/authConfig.js
@@ -7,7 +7,7 @@ require("dotenv").config({ path: "src/data/.env" });
 module.exports = (app) => {
     app.use((req, res, next) => {
         const headers = req.headers;
-        if (headers.authorization && headers.authorization === process.env.AUTHORIZATION) {
+        if (headers?.authorization === process.env.AUTHORIZATION) {
             next();
         } else {
             return res.status(401).send({
